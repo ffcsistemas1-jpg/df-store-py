caff262423706b52c1e7e982daa38250909b7757
ffcsistemas1-jpg <ffcsistemas1@gmail.com>
2026-09-09 14:12:08 -0300

    Aplicar estilo compacto a la ficha de producto

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import "./premium-storefront.css";
 import "./mobile-premium.css";
 import "./storefront-compact.css";
 import "./nova-style.css";
+import "./product-page-polish.css";
 import { CartProvider, Header, WhatsAppButton, TopBanner, MetaPixel, ServiceWorkerRegister, SiteFooter, MobileCustomerNav } from "./ui";
 
 export const metadata: Metadata = {
